5ca91a4d4fa088c2c09bb2be29d4b91bece5fa7b
PajiloyMangust <podoprigoravadim@rambler.ru>
2026-01-31 14:48:18 +0500

    fixed migrations names

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,2 +1,14 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS apps;
+CREATE TABLE IF NOT EXISTS users
+(
+    id        INT PRIMARY KEY,
+    email     TEXT NOT NULL UNIQUE,
+    pass_hash BLOB NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_email ON users(email);
+
+CREATE TABLE IF NOT EXISTS apps
+(
+    id INT PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    secret TEXT NOT NULL UNIQUE
+);
